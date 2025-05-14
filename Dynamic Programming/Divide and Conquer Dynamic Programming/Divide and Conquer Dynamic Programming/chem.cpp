4277a4f151ead38788e8b7c84c0aa0983aa22565
AnastasisKol <anastasiskol@gmail.com>
2025-05-14 06:29:46 +0300

    .

--- Dynamic Programming/Divide and Conquer Dynamic Programming/Divide and Conquer Dynamic Programming/chem.cpp
+++ Dynamic Programming/Divide and Conquer Dynamic Programming/Divide and Conquer Dynamic Programming/chem.cpp
@@ -16,6 +16,22 @@ int C(int i, int j) {
 	return PSV[j - 1][j] - PSV[i - 1][j];
 }
 
+void ComputeDP(int j, int lo, int hi, int opt_lo, int opt_hi) {
+	if (lo > hi)
+		return;
+	int mid = (lo + hi) / 2;
+	dp[j % 2][mid] = INF;
+	int opt_i;
+	for (int i = opt_lo; i <= min(opt_hi, mid); ++i) {
+		if (dp[(j - 1) % 2][i - 1] + C(i, mid) < dp[j % 2][mid]) {
+			dp[j % 2][mid] = dp[(j - 1) % 2][i - 1] + C(i, mid);
+			opt_i = i;
+		}
+	}
+	ComputeDP(j, lo, mid - 1, opt_lo, opt_i);
+	ComputeDP(j, mid + 1, hi, opt_i, opt_hi);
+}
+
 int main() {
 	scanf("%d %d", &N, &K);
 	for (int i = 1; i <= N; ++i) {
@@ -32,12 +48,7 @@ int main() {
 	dp[0][0] = 0;
 	for (int i = 1; i <= N; ++i)
 		dp[0][i] = INF;
-	for (int j = 1; j <= K; ++j) {
-		for (int i = 1; i <= N; ++i) {
-			dp[j % 2][i] = INF;
-			for (int k = 1; k <= i; ++k)
-				dp[j % 2][i] = min(dp[j % 2][i], dp[(j - 1) % 2][k - 1] + C(k, i));
-		}
-	}
+	for (int j = 1; j <= K; ++j)
+		ComputeDP(j, 1, N, 1, N);
 	printf("%d\n", dp[K % 2][N]);
 }
