31aef4c1da7a224469b7ab2925c5727d90fb5e58
AnastasisKol <anastasiskol@gmail.com>
2025-05-25 04:57:52 +0300

    .

--- Divide and Conquer/Divide and Conquer/estellessuperbox.cpp
+++ Divide and Conquer/Divide and Conquer/estellessuperbox.cpp
@@ -25,23 +25,48 @@ int T;
 void DivideAndConquer(int lo, int hi, vector<query> Q) {
 	if (lo == hi) {
 		for (int i = 0; i < Q.size(); ++i)
-			B[Q[i].idx] = S[lo] * (C[lo] > Q[i].X);
+			B[Q[i].idx] = S[lo] * (C[lo] <= Q[i].X);
 		return;
 	}
 	int mid = (lo + hi) / 2;
-	for (int x = 1; x <= MAXX; ++x)
+	for (int x = 0; x <= MAXX; ++x)
 		dpLeft[mid + 1][x] = 0;
-	for (int i = )
+	for (int i = lo; i <= mid; ++i)
+		dpLeft[i][0] = 0;
 	for (int i = mid; i >= lo; --i) {
 		for (int x = 1; x <= MAXX; ++x) {
 			dpLeft[i][x] = dpLeft[i + 1][x];
 			if (C[i] <= x)
-				dpLeft[i][x] = max(dpLeft[i][x], dpLeft[i + 1][x - C[i]]);
+				dpLeft[i][x] = max(dpLeft[i][x], dpLeft[i + 1][x - C[i]] + S[i]);
 		}
 	}
+	for (int x = 0; x <= MAXX; ++x)
+		dpRight[mid][x] = 0;
+	for (int i = mid + 1; i <= hi; ++i)
+		dpRight[i][0] = 0;
 	for (int i = mid + 1; i <= hi; ++i) {
-
+		for (int x = 1; x <= MAXX; ++x) {
+			dpRight[i][x] = dpRight[i - 1][x];
+			if (C[i] <= x)
+				dpRight[i][x] = max(dpRight[i][x], dpRight[i - 1][x - C[i]] + S[i]);
+		}
 	}
+	vector<query> QL;
+	vector<query> QR;
+	for (int i = 0; i < Q.size(); ++i) {
+		if (Q[i].L <= mid && mid < Q[i].R) {
+			for (int x = 0; x <= Q[i].X; ++x)
+				B[Q[i].idx] = max(B[Q[i].idx], dpLeft[Q[i].L][x] + dpRight[Q[i].R][Q[i].X - x]);
+		}
+		else if (Q[i].R <= mid) {
+			QL.push_back(Q[i]);
+		}
+		else {
+			QR.push_back(Q[i]);
+		}
+	}
+	DivideAndConquer(lo, mid, QL);
+	DivideAndConquer(mid + 1, hi, QR);
 }
 
 int main() {
@@ -52,6 +77,11 @@ int main() {
 	vector<query> Q(T);
 	for (int i = 0; i < T; ++i) {
 		scanf("%d %d %d", &Q[i].L, &Q[i].R, &Q[i].X);
+		--Q[i].L;
+		--Q[i].R;
 		Q[i].idx = i;
 	}
+	DivideAndConquer(0, N - 1, Q);
+	for (int i = 0; i < T; ++i) 
+		printf("%lld\n", B[i]);
 }
