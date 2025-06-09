1d3c9ad36931264c628c694672bdc87968c3a2e2
AnastasisKol <anastasiskol@gmail.com>
2025-06-09 04:46:45 +0300

    .

--- Divide and Conquer/Divide and Conquer/visiblebuildingsqueries.cpp
+++ Divide and Conquer/Divide and Conquer/visiblebuildingsqueries.cpp
@@ -14,11 +14,24 @@ struct query {
 };
 
 vector<int> H(MAXN);
-vector<int> B(MAXN);
+vector<int> B(MAXT);
 vector<int> VL(MAXN);
 vector<int> VR(MAXN);
+vector<int> maxLeftHeightUntil(MAXN);
+vector<int> maxRightHeightUntil(MAXN);
 int N;
 int T;
+
+int BinarySearch(int lo, int hi, int i) {
+	while (lo < hi) {
+		int mid = (lo + hi) / 2;
+		if (maxRightHeightUntil[mid] > maxLeftHeightUntil[i])
+			hi = mid;
+		else
+			lo = mid + 1;
+	}
+	return maxRightHeightUntil[lo] > maxLeftHeightUntil[i] ? lo : -1;
+}
 
 void DivideAndConquer(int lo, int hi, vector<query> Q) {
 	if (lo == hi) {
@@ -28,33 +41,40 @@ void DivideAndConquer(int lo, int hi, vector<query> Q) {
 	}
 	int mid = (lo + hi) / 2;
 	stack<int> S;
-	int maxHeight = 0;
+	maxLeftHeightUntil[mid + 1] = 0;
 	for (int i = mid; i >= lo; --i) {
 		while (!S.empty() && H[i] >= H[S.top()])
 			S.pop();
 		S.push(i);
 		VL[i] = S.size();
-		maxHeight = max(maxHeight, H[i]);
+		maxLeftHeightUntil[i] = max(maxLeftHeightUntil[i + 1], H[i]);
 	}
 	int totalVisible = 0;
+	maxRightHeightUntil[mid] = 0;
+	VR[mid] = 0;
 	for (int i = mid + 1; i <= hi; ++i) {
-		if (H[i] > maxHeight) {
+		if (H[i] > maxRightHeightUntil[i - 1]) {
 			VR[i] = ++totalVisible;
-			maxHeight = H[i];
+			maxRightHeightUntil[i] = H[i];
 		}
 		else {
 			VR[i] = totalVisible;
+			maxRightHeightUntil[i] = maxRightHeightUntil[i - 1];
 		}
 	}
 	vector<query> QL;
 	vector<query> QR;
 	for (int i = 0; i < Q.size(); ++i) {
-		if (Q[i].L <= mid && mid < Q[i].R)
-			B[Q[i].idx] = VL[Q[i].L] + VR[Q[i].R];
-		else if (Q[i].R <= mid)
+		if (Q[i].L <= mid && mid < Q[i].R) {
+			int p = BinarySearch(mid + 1, Q[i].R, Q[i].L);
+			B[Q[i].idx] = VL[Q[i].L] + (p != -1 ? VR[Q[i].R] - VR[p - 1] : 0);
+		}
+		else if (Q[i].R <= mid) {
 			QL.push_back(Q[i]);
-		else
+		}
+		else {
 			QR.push_back(Q[i]);
+		}
 	}
 	DivideAndConquer(lo, mid, QL);
 	DivideAndConquer(mid + 1, hi, QR);
