b6b114b9c2849b80c133e052ac62f48777cbe8db
AnastasisKol <anastasiskol@gmail.com>
2025-08-16 19:01:59 +0300

    .

---
+++ Dynamic Programming/Segment Tree Optimization/Segment Tree Optimization/boats.cpp
@@ -0,0 +1,64 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#define MAXN 500000
+using namespace std;
+
+struct couple {
+	int A;
+	int B;
+};
+
+bool compareCouples(couple X, couple Y) {
+	return X.A < Y.A || X.A == Y.A && X.B < Y.B;
+}
+
+set<pair<int, int>> S;
+vector<couple> C(MAXN);
+vector<int> P(MAXN);
+vector<int> T(4 * MAXN);
+vector<int> dp(MAXN);
+int N;
+
+void Update(int lo, int hi, int v, int i, int x) {
+	if (lo == hi) {
+		T[v] = x;
+		return;
+	}
+	int mid = (lo + hi) / 2;
+	if (i <= mid)
+		Update(lo, mid, 2 * v + 1, i, x);
+	else
+		Update(mid + 1, hi, 2 * v + 2, i, x);
+	T[v] = max(T[2 * v + 1], T[2 * v + 2]);
+}
+
+int Query(int lo, int hi, int v, int qlo, int qhi) {
+	if (qlo <= lo && hi <= qhi)
+		return T[v];
+	if (qlo > hi || qhi < lo)
+		return 0;
+	int mid = (lo + hi) / 2;
+	int maxLeft = Query(lo, mid, 2 * v + 1, qlo, qhi);
+	int maxRight = Query(mid + 1, hi, 2 * v + 2, qlo, qhi);
+	return max(maxLeft, maxRight);
+}
+
+int main() {
+	scanf("%d", &N);
+	for (int i = 0; i < N; ++i) 
+		scanf("%d %d", &C[i].A, &C[i].B);
+	sort(C.begin(), C.begin() + N, compareCouples);
+	for (int i = 0; i < N; ++i)
+		S.insert({ C[i].B, i });
+	int i = 0;
+	for (auto [value, index] : S)
+		P[index] = i++;
+	for (int i = N - 1; i >= 0; --i) {
+		dp[i] = Query(0, N - 1, 0, P[i] + 1, N - 1) + 1;
+		Update(0, N - 1, 0, P[i], dp[i]);
+	}
+	printf("%d\n", *max_element(dp.begin(), dp.begin() + N));
+}
