d3dfc9b620708daa01d588edadf463db2b25e4ca
AnastasisKol <anastasiskol@gmail.com>
2025-09-04 21:07:22 +0300

    .

---
+++ Search Methods/Binary Search/Parallel Binary Search/Parallel Binary Search/roadtrip.cpp
@@ -0,0 +1,84 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#define MAXN 30000
+#define MAXM 200000
+#define MAXQ 100000
+using namespace std;
+
+struct edge {
+	int v;
+	int u;
+	int d;
+};
+
+bool compareEdges(edge A, edge B) {
+	return A.d < B.d;
+}
+
+vector<edge> E(MAXM + 1);
+vector<pair<int, int>> T(MAXQ + 1);
+vector<pair<int, int>> R(MAXQ + 1);
+vector<vector<int>> TS(MAXM + 1);
+vector<int> P(MAXN + 1);
+vector<int> S(MAXN + 1);
+int N;
+int M;
+int Q;
+
+void DSU() {
+	for (int a = 1; a <= N; ++a) {
+		P[a] = a;
+		S[a] = 1;
+	}
+}
+
+int Find(int a) {
+	if (P[a] == a)
+		return a;
+	return P[a] = Find(P[a]);
+}
+
+void Unite(int a, int b) {
+	a = Find(a);
+	b = Find(b);
+	if (S[b] > S[a])
+		swap(a, b);
+	P[b] = a;
+	S[a] += S[b];
+}
+
+int main() {
+	scanf("%d %d", &N, &M);
+	for (int i = 1; i <= M; ++i)
+		scanf("%d %d %d", &E[i].v, &E[i].u, &E[i].d);
+	scanf("%d", &Q);
+	for (int i = 1; i <= Q; ++i)
+		scanf("%d %d", &T[i].first, &T[i].second);
+	sort(E.begin() + 1, E.begin() + M + 1, compareEdges);
+	for (int i = 1; i <= Q; ++i)
+		R[i] = { 1, M };
+	for (int i = 0; i < ceil((double)log2(M)); ++i) {
+		for (int j = 1; j <= M; ++j)
+			TS[j].clear();
+		for (int j = 1; j <= Q; ++j) {
+			auto [lo, hi] = R[j];
+			if (lo < hi)
+				TS[(lo + hi) / 2].push_back(j);
+		}
+		DSU();
+		for (int j = 1; j <= M; ++j) {
+			Unite(E[j].v, E[j].u);
+			for (int k : TS[j]) {
+				if (Find(T[k].first) == Find(T[k].second)) 
+					R[k].second = j;
+				else 
+					R[k].first = j + 1;
+			}
+		}
+	}
+	for (int i = 1; i <= Q; ++i)
+		printf("%d\n", E[R[i].first].d);
+}
