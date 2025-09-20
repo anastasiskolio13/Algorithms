0b9d6cef67dcbbeb187129d12a05ea1af1c74420
AnastasisKol <anastasiskol@gmail.com>
2025-09-20 16:02:11 +0300

    .

--- Segment Trees/Lazy Propagation/Lazy Propagation/rangeupdatequeries.cpp
+++ Segment Trees/Lazy Propagation/Lazy Propagation/rangeupdatequeries.cpp
@@ -1,9 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <vector>
-#define MAXN 100000
+#define MAXN 200000
 using namespace std;
 
+vector<int> X(MAXN);
 vector<long long> T(4 * MAXN);
 vector<long long> L(4 * MAXN);
 int N;
@@ -14,15 +15,15 @@ void lazyUpdate(int lo, int hi, int v, int qlo, int qhi, int x) {
 		T[v] += L[v];
 		if (lo != hi) {
 			L[2 * v + 1] += L[v];
-			L[2 * v + 1] += L[v];
+			L[2 * v + 2] += L[v];
 		}
 		L[v] = 0;
 	}
 	if (qlo <= lo && hi <= qhi) {
 		T[v] += x;
 		if (lo != hi) {
-			L[2 * v + 1] += L[v];
-			L[2 * v + 2] += L[v];
+			L[2 * v + 1] += x;
+			L[2 * v + 2] += x;
 		}
 		return;
 	}
@@ -52,7 +53,21 @@ long long lazyQuery(int lo, int hi, int v, int qlo, int qhi) {
 	return sumLeft + sumRight;
 }
 
-
 int main() {
 	scanf("%d %d", &N, &Q);
+	for (int i = 0; i < N; ++i) {
+		scanf("%d", &X[i]);
+		lazyUpdate(0, N - 1, 0, i, i, X[i]);
+	}
+	while (Q--) {
+		int a, b, c, d;
+		scanf("%d %d", &a, &b);
+		if (a == 1) {
+			scanf("%d %d", &c, &d);
+			lazyUpdate(0, N - 1, 0, b - 1, c - 1, d);
+		}
+		else {
+			printf("%lld\n", lazyQuery(0, N - 1, 0, b - 1, b - 1));
+		}
+	}
 }
